fb86969e5a1f084bd40ee8e47cfcf11c57e8e9f7
Kenneth Jiang <k3nyyj@gmail.com>
2017-09-25 02:56:14 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,26 @@
+-- CREATE TABLE bears (
+--   id INTEGER PRIMARY KEY,
+--   name TEXT,
+--   age INTEGER,
+--   gender char,
+--   color TEXT,
+--   temperament TEXT,
+--   alive BOOLEAN
+-- );
+--
+-- Mr. Chocolate
+-- Rowdy
+-- Tabitha
+-- Sergeant Brown
+-- Melissa
+-- Grinch
+-- Wendy
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 1, "M", "Red", "Happy", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 2, "M", "Blue", "Sad", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 3, "F", "Yellow", "Neutral", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 4, "M", "Green", "Estatic", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 5, "M", "Orange", "Depressed", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 6, "M", "Grey", "Angry", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 7, "F", "Purple", "Energetic", "true");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 8, "F", "Black", "Enraged", "true");
